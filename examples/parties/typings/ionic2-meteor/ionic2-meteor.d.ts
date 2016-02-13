55b796175412ff70f7971f4b96555d91f087ddf4
muffasa <ohadgalor1990@gmail.com>
2016-02-13 16:11:27 +0200

    typings and versions removed

--- examples/parties/typings/ionic2-meteor/ionic2-meteor.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../angular2/angular2" />
-/// <reference path="ionic-framework/ionic" />
-
-declare module 'ionic2-meteor' {
-  export function MeteorApp(args?: {}): (cls: core.Type) => any;
-}
